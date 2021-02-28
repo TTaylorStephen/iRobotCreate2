dd91e633585336a1dbf34fdb45f7903478a10d5e
Stephen Taylor <TTaylorStephen@gmail.com>
2021-02-27 22:55:51 -0500

    updated teleop_node.py

--- teleop/src/teleop_node.py
+++ teleop/src/teleop_node.py
@@ -2,7 +2,9 @@
 import rospy
 import pygame 
 import numpy
+import os
 from std_msgs.msg import Int32MultiArray
+from std_msgs.msg import Float32MultiArray
 
 '''
 Map for XBox One S controller is as follows:
@@ -11,17 +13,19 @@ Map for XBox One S controller is as follows:
 		tuple where: -left/down = (-1,-1), right/up = (1,1), no input = (0,0)
 		
 	# buttons: bool values = 1, 0
-		A=0, B=1, Y=4, X=3, l bumper=6, r bumper=7, start=11, left push=13, right push=14
+		A=0, B=1, Y=3, X=2, l bumper=4, r bumper=5, select=6, start=7, left push=8, right push=9
 		
 	# ball axes & triggers: possible values =-1, 0, 1 
-		lrLeft=0,  udLeft=1, lrRight=2, udRight=3, rTrigger=4, lTrigger=5 
+		lrLeft=0,  udLeft=1, lrRight=3, udRight=4, rTrigger=2, lTrigger=5 
 '''
 		
 class TeleOp:
 	
 	def __init__(self):
-			 
-		self.velPub=rospy.Publisher("/wheel_velocity/commanded", Int32MultiArray, queue_size=30)
+	
+		os.system("rosnode kill /move_base_node") #kill autonmous commands during teleop
+		
+		self.velPub=rospy.Publisher("/wheel_velocity/commanded", Float32MultiArray, queue_size=30) #initialize publishers
 		self.bytePub=rospy.Publisher("/bytes", Int32MultiArray, queue_size=10)
 		
 		pygame.init() # initializes pygame modules
@@ -29,8 +33,8 @@ class TeleOp:
 
 		self.hat=0 	  # d-pad (or joystick hat)
 		self.B=1	  # buttons of interest 
-		self.lBumper=6
-		self.rBumper=7
+		self.lBumper=4
+		self.rBumper=5
 		
 	
 	def getID(self):
@@ -50,15 +54,18 @@ class TeleOp:
 	
 	def joyPub(self):
 	
-		vl, vr = 200, 200 		   	# define teleop velocity
-		v, bytes=[0]*2, [0]*2 	   	# allocate space for arrays that will be published
-		velocity=Int32MultiArray() 	# objects for ros variables
+		vl, vr = 200.0, 200.0 		   	# define teleop velocity
+		v, bytes=[0.0]*2, [0]*2 	   	# allocate space for arrays that will be published
+		velocity=Float32MultiArray() 	# objects for ros variables
 		ros_bytes=Int32MultiArray()
 
 		if self.name=='Xbox Wireless Controller':	
 			joy=pygame.joystick.Joystick(self.joy_id)
 			joy.init()	# initialize object created above
 
+			for i in range(joy.get_numaxes()):
+				axis=joy.get_axis(i)
+				print('buttons active at %d are %s\n' %(i, axis))
 			if joy.get_button(self.B)==1:		# If B is pressed, start sending commands				
 				hat=joy.get_hat(self.hat)		# check hat controls for input	
 				if hat[0]==-1:					# store desired velocity per input
@@ -93,5 +100,5 @@ if __name__=='__main__':
 		op.joyPub()		
 		rate.sleep()
 	
-	pygame.quit()
+	#pygame.quit()
 		
